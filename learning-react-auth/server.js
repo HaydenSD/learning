ee7ee654a1633481581e805af1cf909b8c67fd0a
HaydenSD <haydensd@protonmail.ch>
2016-11-23 08:51:41 -0500

    Added webpack and react

--- learning-react-auth/server.js
+++ learning-react-auth/server.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var stormpath = require('express-stormpath');
-
+var webpack = require('webpack');
+var config = require('./webpack.config');
 var app = express();
+var compiler = webpack(config);
+var path = require('path');
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.use(stormpath.init(app, {
   web: {
     produces: ['application/json']
   }
 }));
+
+app.get('/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/css/bootstrap.min.css'));
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 app.on('stormpath.ready', function () {
   app.listen(3000, 'localhost', function (err) {
---
+++ learning-react-auth/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { IndexRoute, Route, browserHistory } from 'react-router';
+import ReactStormpath, { Router, HomeRoute, LoginRoute, AuthenticatedRoute } from 'react-stormpath';
+
+ReactStormpath.init();
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+  </Router>,
+  document.getElementById('app-container')
+)
